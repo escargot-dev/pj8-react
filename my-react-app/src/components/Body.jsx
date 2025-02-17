5f773dc886c754734caf5697f07f3bc3dab540cc
k.LR <k.lerouic@gmail.com>
2025-02-17 18:50:21 +0100

    Home: gallery photo ->ajouter

--- my-react-app/src/components/Body.jsx
+++ my-react-app/src/components/Body.jsx
@@ -1,7 +1,18 @@
+import { useState, useEffect } from "react";
 import "../components/Cart.css"
 import mainImage from '../assets/images/main-photo.jpeg'
 
 const Body = () => {
+  const [logements, setLogements] = useState([]);
+  
+
+  useEffect(() => {
+    fetch(" http://localhost:8080/api/properties") // Appel API
+      .then((response) => response.json())
+      .then((data) => setLogements(data))
+      .catch((error) => console.error("Erreur de chargement :", error));
+  }, []);
+
   return (
     <div className="cart-container">
       <div className="banner">
@@ -11,12 +22,12 @@ const Body = () => {
 
       {/* Galerie des logements */}
       <div className="gallery">
-       {/* {logements.map((logement) => (
+        {logements.map((logement) => (
           <div key={logement.id} className="card">
-            <img src={logement.image} alt={logement.title} className="card-img" />
+            <img src={logement.cover} alt={logement.title} className="card-img" />
             <h2 className="card-title">{logement.title}</h2>
           </div>
-        ))}*/}
+        ))}
       </div>
     </div>
   );
