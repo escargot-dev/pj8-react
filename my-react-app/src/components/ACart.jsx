6cf2b6ebbf58c7a971813a1da24473a8e05a9065
k.LR <k.lerouic@gmail.com>
2025-02-19 23:58:13 +0100

    ajouter data.jspn pour collapse et ameliore le style css

--- my-react-app/src/components/ACart.jsx
+++ my-react-app/src/components/ACart.jsx
@@ -1,28 +1,38 @@
+import React, { useState } from "react";
 import "../components/A-cart.css"
 import  ChevronDown  from "../assets/icones/dropdown-icone.svg";
 import  cartImage  from "../assets/images/a-props-photo.jpeg";
+import collapsibleData from "../data/collapsibleData.json";
 
 const ACart = () => {
-  console.log("ACart est rendu");
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const toggleCollapse = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
 
   return (
     <div className="a-cart-container">
       {/* Image */}
-      <img
-        src={cartImage}
-        alt="À propos"
-        className="a-cart-image"
-      />
+      <img src={cartImage} alt="À propos" className="a-cart-image" />
 
       {/* Groupe de barres */}
       <div className="a-cart-bars">
-        {["Fiabilité", "Respect", "Service", "Sécurité"].map((title, index) => (
+        {collapsibleData.map((section, index) => (
           <div key={index} className="a-cart-bar">
-            <span className="a-cart-title">{title}</span>
-            <img src={ChevronDown} alt="Icône dropdown" className="a-cart-icon" />
+            <span className="a-cart-title">{section.title}</span>
+            <div onClick={() => toggleCollapse(index)}>
+              <img src={ChevronDown} alt="Icône dropdown" className={`a-cart-icon ${openIndex === index ? "rotate" : ""}`} />
+            </div>
+
+            {openIndex === index && (
+              <div className={`a-cart-content ${openIndex === index ? "open" : ""}`}>
+                <p>{section.content}</p>
+              </div>
+            )}
           </div>
         ))}
-      </div>
+       </div>
     </div>
   );
 };
